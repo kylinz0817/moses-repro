0d16859229f89843360f11640af1ffeb55eb4ac9
kylinz0817 <j4tan@uw.edu>
2026-03-09 11:41:49 -0700

    Delete scripts/run_additional_experiments.sh

--- scripts/run_additional_experiments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-python experiments/ablations/training_size.py --dataset CMV SciXGen WP XSum --detector roberta
-python experiments/ablations/pca_dims.py --dataset CMV SciXGen WP XSum --detector roberta
-python experiments/ablations/neighborhood_k.py --dataset CMV SciXGen WP XSum --detector roberta
-python experiments/hyperparameter_tuning/tune_hyperparams.py --dataset CMV SciXGen --detector roberta --param all
